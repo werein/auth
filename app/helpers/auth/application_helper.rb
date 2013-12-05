8267af409eaf82a191d6fe943f5daabf8a7a1cfb
JiriKolarik <jiri.kolarik@imin.cz>
2013-12-05 15:57:31 +0100

    Renamed attributes

--- app/helpers/auth/application_helper.rb
+++ app/helpers/auth/application_helper.rb
@@ -13,17 +13,17 @@ module Auth
         klass, path = object.class, object.class
       end
       if (can? :create, klass) || (can? :edit, klass) || (can? :delete, klass)
-        hide = options.fetch(:hide, [])
+        except = options.fetch(:except, [])
         content_tag :div, class: "manage btn-group-vertical #{options[:class]}", style: ('display:none' unless manage?) do
           res = ''
           res += link_to glyphicon('pencil'), new_polymorphic_path(path),
-              'data-no-turbolink' => true, class: "btn btn-default" unless hide.include?(:new)
+              'data-no-turbolink' => true, class: "btn btn-default" unless except.include?(:new)
           res += link_to glyphicon('edit'), edit_polymorphic_path(object), 
-              'data-no-turbolink' => true, class: "btn btn-default" if can? :edit, klass and !hide.include?(:edit)
+              'data-no-turbolink' => true, class: "btn btn-default" if can? :edit, klass and !except.include?(:edit)
           res += link_to glyphicon('remove'), object, data: { confirm: t('misc.are_you_sure') }, method: :delete, remote: (true if options[:remote]),
-              class: "btn btn-danger" if can? :delete, klass and !hide.include?(:delete)
+              class: "btn btn-danger" if can? :delete, klass and !except.include?(:delete)
           res += link_to glyphicon('list'), polymorphic_path(path), 
-              class: "btn btn-info " if can? :show, klass and !hide.include?(:list)
+              class: "btn btn-info " if can? :show, klass and !except.include?(:list)
           res.html_safe
         end
       end
