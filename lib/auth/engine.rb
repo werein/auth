7be396c4b0ada66e27d8306e8dac7cecbe1434e2
JiriKolarik <jiri.kolarik@imin.cz>
2014-01-04 17:46:06 +0100

    include helper doesn’t work in production

--- lib/auth/engine.rb
+++ lib/auth/engine.rb
@@ -17,6 +17,10 @@ module Auth
       Decorators.register! Engine.root, Rails.root
     end
 
+    initializer :append_views do
+      ActionView::Base.send :include, ApplicationHelper
+    end if Rails.env.production?
+
     initializer :append_migrations do |app|
       unless app.root.to_s.match root.to_s
         config.paths["db/migrate"].expanded.each do |expanded_path|
