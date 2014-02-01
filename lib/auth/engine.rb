01dc232d5719f118205e62c03f41e10c1ced10ba
JiriKolarik <jiri.kolarik@imin.cz>
2014-02-01 15:25:36 +0100

    Include helper in initializer

--- lib/auth/engine.rb
+++ lib/auth/engine.rb
@@ -19,7 +19,7 @@ module Auth
 
     initializer :append_views do
       ActionView::Base.send :include, ApplicationHelper
-    end if Rails.env.production?
+    end
 
     initializer :append_migrations do |app|
       unless app.root.to_s.match root.to_s
