2342a93e6190e76bceaf55311ba8f8c44b57298a
JiriKolarik <jiri.kolarik@imin.cz>
2014-02-02 14:45:47 +0100

    Better translations in helper

--- app/helpers/auth/application_helper.rb
+++ app/helpers/auth/application_helper.rb
@@ -2,7 +2,7 @@ module Auth
   module ApplicationHelper
 
     def create_button klass, options = {}
-      link_to gt('pencil', "#{klass.model_name.i18n_key.to_s.gsub('/', '.')}.new"), new_polymorphic_path(klass), style: ('display:none' unless manage?), class: "manage btn btn-info btn-text btn-create", 'data-no-turbolink' => true if can? :create, klass
+      link_to gt('pencil', t('auth.helper.create_button.create', model: klass.model_name.human.downcase)), new_polymorphic_path(klass), style: ('display:none' unless manage?), class: "manage btn btn-info btn-text btn-create", 'data-no-turbolink' => true if can? :create, klass
     end
 
     def manage_icons object, options = {}
@@ -14,7 +14,7 @@ module Auth
       end
       if (can? :create, klass) || (can? :edit, klass) || (can? :delete, klass)
         except = options.fetch(:except, [])
-        content_tag :div, class: "manage btn-group-vertical #{options[:class]}", style: ('display:none' unless manage?) do
+        content_tag :div, class: "manage btn-group-vertical btn-manage #{options[:class]}", style: ('display:none' unless manage?) do
           res = ''
           res += link_to glyphicon('pencil'), new_polymorphic_path(path),
               'data-no-turbolink' => true, class: "btn btn-default" unless except.include?(:new)
@@ -37,15 +37,15 @@ module Auth
         klass, path = object.class, object.class
       end
       if (can? :create, klass) ||(can? :edit, klass) || (can? :delete, klass)
-        content_tag :div, class: "manage btn-group #{options[:group_class]}", style: ('display:none' unless manage?) do
+        content_tag :div, class: "manage btn-group btn-manage #{options[:class]}", style: ('display:none' unless manage?) do
           res = ''
-          res += link_to gt('pencil', 'misc.create'), new_polymorphic_path(path),
+          res += link_to gt('pencil', t('auth.helper.create', model: klass.model_name.human.downcase)), new_polymorphic_path(path),
               'data-no-turbolink' => true, class: "btn btn-default btn-text" unless options[:new]
-          res += link_to gt('edit', 'misc.edit'), edit_polymorphic_path(object), 
+          res += link_to gt('edit', t('auth.helper.edit', model: klass.model_name.human.downcase)), edit_polymorphic_path(object), 
               'data-no-turbolink' => true, class: "btn btn-default btn-text" if can? :edit, klass and !options[:edit]
-          res += link_to gt('remove','misc.delete'), object, data: { confirm: t('misc.are_you_sure') }, method: :delete, remote: (true if options[:remote]),
+          res += link_to gt('remove', t('auth.helper.destroy', model: klass.model_name.human.downcase)), object, data: { confirm: t('misc.are_you_sure') }, method: :delete, remote: (true if options[:remote]),
               class: "btn btn-danger btn-text" if can? :delete, klass and !options[:delete]
-          res += link_to gt('list-alt', 'misc.all'), polymorphic_path(path), 
+          res += link_to gt('list-alt', t('auth.helper.view', model: klass.model_name.human.downcase)), polymorphic_path(path), 
               class: "btn btn-info btn-text" if (can? :edit, klass) and !options[:list]
           res.html_safe
         end
